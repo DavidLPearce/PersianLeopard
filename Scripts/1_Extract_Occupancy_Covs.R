e59d2a76f4a786cb3774f21cae91a247b79dcfa6
David Pearce <davepearce5819@gmail.com>
2025-05-22 14:42:11 -0500

    Extract values in concentric buffers (rings).

--- Scripts/1_Extract_Occupancy_Covs.R
+++ Scripts/1_Extract_Occupancy_Covs.R
@@ -62,7 +62,7 @@ plot(villages)
 # Vector ruggedness measure to raster
 VRM <- spatialEco::vrm(DEM, s = 3) # Extract VRM # 3x3 window
 plot(VRM)
-VRM_rast <- VRM > 0.05 & VRM <= 0.20# Classify VRM values into raster
+VRM_rast <- VRM > 0.03 & VRM <= 1 # Classify VRM values into raster
 plot(VRM_rast)
 
 
@@ -73,56 +73,45 @@ plot(VRM_rast)
 #
 # ------------------------------------------------------------------------------
 
-# Sequence of buffers
-buffer_vec <-  seq(250, 15000, by = 250) 
-sigma_vec <- seq(250, 15000, by = 250) 
-
 # Dataframe for covariates
 dist_df <- as.data.frame(sites)
 dist_df <- dist_df[,-c(4:5)]
 colnames(dist_df)[1] <- "SiteID"
 print(dist_df)
 
-elev_df <- cov_df
-slope_df <- cov_df
-aspect_df <- cov_df
-VRM_df <- cov_df
-VRM_AI_df <- cov_df
-Vrm_Patch_df <- cov_df
-Npast_df <- cov_df
-Nvil_df <- cov_df
-Nriver_df <- cov_df
+elev_df <- dist_df
+slope_df <- dist_df
+aspect_df <- dist_df
+VRM_df <- dist_df
+VRM_AI_df <- dist_df
+VRM_Patch_df <- dist_df
+Npast_df <- dist_df
+Nvil_df <- dist_df
+Nriver_df <- dist_df
 
-# Distance-weighted function
-weighted_metric <- function(site_coords, raster_df, value_col, sigma) {
-  if (nrow(raster_df) == 0) return(NA)
-  dists <- sqrt((raster_df$x - site_coords[1])^2 + (raster_df$y - site_coords[2])^2)
-  weights <- exp(-(dists^2) / (2 * sigma^2))
-  weighted.mean(raster_df[[value_col]], weights, na.rm = TRUE)
-}
+# Define concentric buffers aka rings
+ring_vec <- seq(250, 15000, by = 250)
 
 # Initialize the progress bar
-total_steps <- NROW(sites) * length(buffer_vec) * length(sigma_vec)
+total_steps <- NROW(sites) * (length(ring_vec) - 1)
 pb <- progress_bar$new(
-  format = "Processing [:bar] :percent | :message | Elapsed: :elapsed",
-  total = total_steps,
-  clear = FALSE,
-  width = 120
+  format = "  Processing [:bar] :percent | Site :site Ring :ring",
+  total = total_steps, clear = FALSE, width = 80
 )
 
-# row = 1
-# buffer = 1
-# sigma = 1
+row = 1
+ring_idx = 2
 
-# Loop
+# ---------------------
+# Extract Metrics
+# ---------------------
 for (row in 1:NROW(sites)) {
-
+  
   # Subset site
   site_sub <- sites[row]
   
-  # Extract site coordinates
-  site_coords <- as.data.frame(site_sub)
-  site_coords <- c(site_coords$x[1], site_coords$y[1])
+  site_id <- as.data.frame(site_sub)
+  site_id <- site_id[, "Field1"]
   
   # ---------------------
   # Distance Metrics
@@ -147,133 +136,135 @@ for (row in 1:NROW(sites)) {
   dist_df[row, nearest_riv_dist_col] <- nearest_riv_dist
   
   # ---------------------
-  # Buffer Metrics 
+  # Ring Metrics 
   # ---------------------
-
-  for (buffer_idx in seq_along(buffer_vec)){
+  for (ring_idx in 2:length(ring_vec)) {
     
-    # buffer size
-    buffer_size <- buffer_vec[buffer_idx]
+    # Extract ring limits
+    inner_r <- ring_vec[ring_idx - 1]
+    outer_r <- ring_vec[ring_idx]
+        
+    # Progress Bar Update
+    pb$tick(tokens = list(site = site_id, 
+                          ring = paste0(inner_r, "-", outer_r, "m")))
     
-    # Print progress message
-    if (!pb$finished) {
-      pb$message(sprintf("Site: %s | Buffer: %sm", 
-                         sites$Field1[row], buffer_size))
-      pb$tick()
-    }
+    # Create ring
+    outer_buffer <- terra::buffer(site_sub, width = outer_r)
+    inner_buffer <- terra::buffer(site_sub, width = inner_r)
+    ring_polygon <- erase(outer_buffer, inner_buffer)
+    # plot(ring_polygon)
 
-    # Create a buffer around the site
-    site_buffer <- terra::buffer(site_sub, width = buffer_size)
-  
     # Subset DEM
-    DEM_subset <- terra::crop(DEM, site_buffer)
+    DEM_subset <- terra::crop(DEM, outer_buffer)
     # plot(DEM_subset)
     
     # Subset VRM raster
-    VRM_subset <- terra::crop(VRM_rast, site_buffer)
+    VRM_subset <- terra::crop(VRM_rast, outer_buffer)
     # plot(VRM_subset)
+    VRM_ring <- mask(crop(VRM_subset, ring_polygon), ring_polygon)
+    # plot(VRM_ring)
     
+    # Extract Elevation
+    elev_value <- extract(DEM_subset, ring_polygon, fun = mean, na.rm = TRUE)
+    elev_col <- paste0("elev_", outer_r)
+    elev_df[row, elev_col] <- elev_value[,"DEM20kmbuffer"]
+    
+    # Extract Slope
+    slope <- terrain(DEM_subset, v = "slope", unit = "degrees")
+    slope_value <- extract(slope, ring_polygon, fun = mean, na.rm = TRUE)
+    slope_col <- paste0("slope_", outer_r)
+    slope_df[row, slope_col] <- slope_value[,"slope"]
+    
+    
+    # Extract Aspect
+    aspect <- terra::terrain(x = DEM_subset, v = "aspect")
+    aspect_value <- extract(aspect, ring_polygon, fun = mean, na.rm = TRUE)
+    aspect_col <- paste0("aspect_", outer_r)
+    aspect_df[row, aspect_col] <- aspect_value[,"aspect"]
+    
+    # Extract Vector Ruggedness Measure
+    VRM <- spatialEco::vrm(DEM_subset, s = 3) # 3x3 window
+    # plot(VRM)
+    VRM_value <- extract(VRM, ring_polygon, fun = mean, na.rm = TRUE)
+    VRM_col <- paste0("VRM_", outer_r)
+    VRM_df[row, VRM_col] <- VRM_value[, "lyr1"]
     
     # Extract VRM Aggregation Index
     # lsm_c_ai: equals the number of like adjacencies divided by the theoretical maximum possible number of like adjacencies for that class
     # Equals 0 for maximally disaggregated and 100 for maximally aggregated classes.
-    VRM_ai <- landscapemetrics::calculate_lsm(VRM_subset, what = "lsm_c_ai")
+    VRM_ai <- landscapemetrics::calculate_lsm(VRM_ring, what = "lsm_c_ai")
     VRM_ai <- VRM_ai[which(VRM_ai$class == 1),] # VRM
-    VRM_ai_col <- paste0("VRM_ai_", buffer_size, "m")
+    VRM_ai_col <- paste0("VRM_AI_", outer_r)
     if (nrow(VRM_ai) > 0) {
-      VRM_AI_df[row, VRM_ai_col] <- VRM_ai$value
+      VRM_AI_df[row, VRM_ai_col] <- VRM_ai[, "value"]
     } else {
       VRM_AI_df[row, VRM_ai_col] <- NA
     }
     
     # Extract VRM Patch Area
     # lsm_p_area: Calculates the area of each patch.(m^2)
-    VRM_Parea <- landscapemetrics::calculate_lsm(VRM_subset, what = "lsm_p_area")
+    VRM_Parea <- landscapemetrics::calculate_lsm(VRM_ring, what = "lsm_p_area")
     VRM_Parea <- VRM_Parea[which(VRM_Parea$class == 1),] # VRM
     VRM_Parea <- mean(VRM_Parea$value)
-    VRM_Parea_col <- paste0("VRM_Parea_", buffer_size, "m")
+    VRM_Parea_col <- paste0("VRM_Parea_", outer_r)
     if (nrow(VRM_ai) > 0) {
-      Vrm_Patch_df[row, VRM_Parea_col] <- VRM_Parea
+      VRM_Patch_df[row, VRM_Parea_col] <- VRM_Parea
     } else {
-      Vrm_Patch_df[row, VRM_Parea_col] <- NA
+      VRM_Patch_df[row, VRM_Parea_col] <- NA
     }
     
     # Pastures within buffer
-    past_in_buf <- terra::intersect(pasture, site_buffer)
-    # plot(site_buffer) # visually inspect
+    past_in_buf <- terra::intersect(pasture, ring_polygon)
+    # plot(ring_polygon) # visually inspect
     # plot(past_in_buf, add = TRUE)
     Npastures <- nrow(past_in_buf)
-    Npastures_col <- paste0("Npastures_", buffer_size, "m")
+    Npastures_col <- paste0("Npastures_", outer_r)
     Npast_df[row, Npastures_col] <- Npastures
-
+    
     # Villages within buffer
-    vil_in_buf <- terra::intersect(villages, site_buffer)
-    # plot(site_buffer) # visually inspect
+    vil_in_buf <- terra::intersect(villages, ring_polygon)
+    # plot(ring_polygon) # visually inspect
     # plot(vil_in_buf, add = TRUE)
     Nvillages <- nrow(vil_in_buf)
-    Nvillages_col <- paste0("Nvillages_", buffer_size, "m")
+    Nvillages_col <- paste0("Nvillages_", outer_r)
     Nvil_df[row, Nvillages_col] <- Nvillages
     
     # Rivers within buffer
-    riv_in_buf <- terra::intersect(rivers, site_buffer)
-    # plot(site_buffer) # visually inspect
+    riv_in_buf <- terra::intersect(rivers, ring_polygon)
+    # plot(ring_polygon) # visually inspect
     # plot(riv_in_buf, add = TRUE)
     Nriver <- nrow(riv_in_buf)
-    Nriver_col <- paste0("Nriver_", buffer_size, "m")
+    Nriver_col <- paste0("Nriver_", outer_r)
     Nriver_df[row, Nriver_col] <- Nriver
-  
-    # ---------------------
-    # Distance-weighted 
-    # ---------------------
-    for (sigma_idx  in seq_along(sigma_vec)){
-    
-      # sigma size
-      sigma <- sigma_vec[sigma_idx ]  
-      
 
-      # Extract Elevation
-      elev_values <- terra::extract(DEM_subset, site_buffer)
-      elev_tmp <- terra::extract(DEM_subset, site_buffer, xy = TRUE)
-      elev <- weighted_metric(site_coords, elev_tmp, "DEM20kmbuffer", sigma = sigma)
-      elev_col <- paste0("elev_", buffer_size, "m_sigma", sigma)
-      elev_df[row, elev_col] <- elev
-  
-      # Extract Slope
-      slope_values <- terra::terrain(x = DEM_subset, v = "slope", unit = "degrees")
-      # plot(slope_values)
-      slope_tmp <- terra::extract(slope_values, site_buffer, xy = TRUE)
-      slope <- weighted_metric(site_coords, slope_tmp, "slope", sigma = sigma)
-      slope_col <- paste0("slope_", buffer_size, "m_sigma", sigma)
-      slope_df[row, slope_col] <- slope
-  
-      # Extract Aspect
-      aspect_values <- terra::terrain(x = DEM_subset, v = "aspect")
-      # plot(aspect_values)
-      aspect_tmp <- terra::extract(aspect_values, site_buffer, xy = TRUE)
-      aspect <- weighted_metric(site_coords, aspect_tmp, "aspect", sigma = sigma)
-      aspect_col <- paste0("aspect_", buffer_size, "m_sigma", sigma)
-      aspect_df[row, aspect_col] <- aspect
-  
-      # Extract Vector Ruggedness Measure
-      VRM_values <- spatialEco::vrm(DEM_subset, s = 3) # 3x3 window
-      # plot(VRM_values)
-      VRM_tmp <- terra::extract(VRM_values, site_buffer, xy = TRUE)
-      VRM <- weighted_metric(site_coords, VRM_df, "lyr1", sigma = sigma)
-      VRM_col <- paste0("VRM_", VRM_tmp, "m_sigma", sigma)
-      VRM_df[row, VRM_col] <- VRM
+  } # end ring loop
+} # end site loop
 
-    } # end sigma
-  } # end buffer
-} # end site
 # --------------------------- End Extraction Loop -----------------------------
 
 # Take a look
-head(cov_df)
-
+head(dist_df)
+head(elev_df)
+head(slope_df)
+head(aspect_df)
+head(VRM_df)
+head(VRM_AI_df)
+head(VRM_Patch_df)
+head(Npast_df)
+head(Nvil_df)
+head(Nriver_df)
 
 
 
 # Export
-saveRDS(cov_df, "Data/OccuCovData/cov_df.rds")
-
+saveRDS(dist_df, "Data/OccuCovData/dist_df.rds")
+saveRDS(elev_df, "Data/OccuCovData/elev_df.rds")
+saveRDS(slope_df, "Data/OccuCovData/slope_df.rds")
+saveRDS(aspect_df, "Data/OccuCovData/aspect_df.rds")
+saveRDS(VRM_df, "Data/OccuCovData/VRM_df.rds")
+saveRDS(VRM_AI_df, "Data/OccuCovData/VRM_AI_df.rds")
+saveRDS(VRM_Patch_df, "Data/OccuCovData/VRM_Patch_df.rds")
+saveRDS(Npast_df, "Data/OccuCovData/Npast_df.rds")
+saveRDS(Nvil_df, "Data/OccuCovData/Nvil_df.rds")
+saveRDS(Nriver_df, "Data/OccuCovData/Nriver_df.rds")
 
